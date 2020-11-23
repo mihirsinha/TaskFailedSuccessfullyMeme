7a9665c2aab80b4ae33a20c71a4ef465b489eb75
mihirsinha <47106737+mihirsinha@users.noreply.github.com>
2020-11-23 10:25:03 +0530

    Add files via upload
    Files

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <windows.h>
+#include <commctrl.h>
+#include <stdio.h>
+#include "resource.h"
+
+HINSTANCE hInst;
+
+BOOL CALLBACK DlgMain(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch(uMsg)
+    {
+    case WM_INITDIALOG:
+    {
+        int a= MessageBox(hwndDlg, "Task Failed Successfully!","Windows",MB_ICONINFORMATION);
+        EndDialog(hwndDlg, 0);
+    }
+    return TRUE;
+
+    case WM_CLOSE:
+    {
+        //EndDialog(hwndDlg, 0);
+    }
+    return TRUE;
+
+    case WM_COMMAND:
+    {
+        switch(LOWORD(wParam))
+        {
+
+        }
+    }
+    return TRUE;
+    }
+    return FALSE;
+}
+
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    hInst=hInstance;
+    InitCommonControls();
+    return DialogBox(hInst, MAKEINTRESOURCE(DLG_MAIN), NULL, (DLGPROC)DlgMain);
+}
